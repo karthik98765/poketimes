30537a7601b3a961e4683f17bf75aa92ae35a35e
karthi <karthi@exm.com>
2020-12-11 13:11:43 +0530

    link, navlink

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>lorem ipsum dolor sit amet consectetur, adipisicing elit, aliquid repellendus, veniam eveniet modi molestias sequi/</p>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>lorem ipsum dolor sit amet consectetur, adipisicing elit, aliquid repellendus, veniam eveniet modi molestias sequi/</p>
+        </div>
+    )
+}
+
+export default Home
